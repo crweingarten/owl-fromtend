2667c38e990f2d610016fd0f67cf2dc03d238850
crweingarten <65681769+crweingarten@users.noreply.github.com>
2023-08-22 18:44:22 -0700

    saving work

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./OwlFrontEnd.css";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import MoneyItemBox from "./components/MoneyItemBox";
 
 function App() {
   return (
-    <div className="App">
-     
+    <div className="owl-frontend">
+      <Container>
+        <Row>
+          <Col xs={4}>
+            <MoneyItemBox type="income" />
+          </Col>
+          <Col xs={4}>
+            <MoneyItemBox type="expense" />
+          </Col>
+          <Col xs={4}>
+            <MoneyItemBox type="savings" />
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
---
+++ src/components/MoneyItemBox.jsx
@@ -0,0 +1,7 @@
+export default function MoneyItemBox(props) {
+  return (
+    <div className="money-item-box">
+      <div className="money-item-type">{props.type}</div>
+    </div>
+  );
+}
